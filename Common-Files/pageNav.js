6f8ddf0ec6b3a5972a24ea3422861b2fa939ea18
Kibur_Yoseph <kiburstgr@gmail.com>
2023-04-29 11:05:48 +0300

    Blog Post Page Complete

--- Common-Files/pageNav.js
+++ Common-Files/pageNav.js
@@ -1,5 +1,8 @@
 const pageNumber = document.querySelectorAll(".pageNumber");
 const list = document.querySelectorAll(".list");
+const commentPageNumber = document.querySelectorAll(".commentPageNumber");
+const commentList = document.querySelectorAll(".commentList");
+
 
 pageNumber.forEach(page => {
     page.addEventListener("click", () => {
@@ -17,6 +20,31 @@ pageNumber.forEach(page => {
                 list[index].dataset.activeList = true;
             setTimeout(()=> {
                     list[index].style.opacity = "1"
+                 }
+                 ,200);
+             }
+             ,200);
+        })
+
+    })
+})
+
+commentPageNumber.forEach(commentPage => {
+    commentPage.addEventListener("click", () => {
+        let commentIndex = [...commentPageNumber].indexOf(commentPage);
+
+        commentPageNumber.forEach(commentPage => {
+            commentPage.style.color = "rgb(192, 192, 192)";
+        })
+        commentPage.style.color = "#0E3D95";
+
+        commentList.forEach(aCommentList => {
+            aCommentList.style.opacity = "0";
+            setTimeout(()=> {
+                delete aCommentList.dataset.activeCommentList;
+                commentList[commentIndex].dataset.activeCommentList = true;
+            setTimeout(()=> {
+                    commentList[commentIndex].style.opacity = "1"
                  }
                  ,200);
              }
